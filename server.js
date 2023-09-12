1dc91d60b4c10647ad86c2121805f66af8c4aeb6
Mac <mac@Macs-MacBook-Pro.local>
2023-09-12 18:44:14 +0100

    Added my project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000; // You can change this port to any available port you want
+
+// Set up a route to serve your single-page site
+app.get('/', (req, res) => {
+  res.send('Welcome to your single-page site!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
