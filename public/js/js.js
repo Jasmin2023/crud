43f74e5a919b6d7d2fe61dfbc1eed5f3f243423e
Jasmin2023 <zainabal2023@gmail.com>
2023-10-24 11:58:52 +0100

    some changes

--- public/js/js.js
+++ public/js/js.js
@@ -1,8 +1,9 @@
-console.log("123");
 //window.localStorage.getItem("token");
+
 document.addEventListener("DOMContentLoaded", function() { 
 
     const loginForm = document.getElementById("loginForm");
+    const loginFormSection = document.querySelector(".loginFormSection");
     const emailInput = document.getElementById("email");
     const passwordInput = document.getElementById("password");
     const messageDiv = document.getElementById("message");
@@ -11,21 +12,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const storedEmail = localStorage.getItem("email");
     const crudAllUsers = document.querySelector(".crudAllUsers");
     const pageHeader = document.querySelector(".pageHeader");
-    const profileTable = document.getElementById("profileTable");
+    const loggedIn = document.querySelector(".loggedIn");
     
     if (token) {
       // If a token exists, show the logout link and hide the login form
-      loginForm.style.display = "none";
+      loginFormSection.style.display = "none";
       messageDiv.style.display = "block";
       messageDiv.textContent = "Hello, " + storedEmail;
       logoutLink.style.display = "block";
-      crudAllUsers.style.display = "block";
       pageHeader.style.display = "block";
+      loggedIn.style.display = "block";
     } else {
-      loginForm.style.display = "block";
-      crudAllUsers.style.display = "none";
+      loginFormSection.style.display = "block";
+      loggedIn.style.display = "none";
       pageHeader.style.display = "none";
     }
+
     if (storedEmail) {
       emailInput.value = storedEmail;
     }
@@ -33,41 +35,45 @@ document.addEventListener("DOMContentLoaded", function() {
       e.preventDefault();
       const emailVal = emailInput.value;
       const passwordVal = passwordInput.value;
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://reqres.in/api/login", true);
-      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4) {
-          if(xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            if(response.token){
-              localStorage.setItem("token", response.token);
-              // Store the email in localStorage
-              localStorage.setItem("email", emailVal);
-              // Display greeting message
-              messageDiv.style.display = "block";
-              messageDiv.textContent = "Hello, " + emailVal;
-              loginForm.style.display = "none";
-              logoutLink.style.display = "block";
-              crudAllUsers.style.display = "block";
-              pageHeader.style.display = "block";
-            } else {
-              // Authentication failed
-              messageDiv.textContent = "Authentication failed. Please try again.";
-              messageDiv.style.display = "block";
-              crudAllUsers.style.display = "none";
-              pageHeader.style.display = "none";
+      if (emailVal === "eve.holt@reqres.in" && passwordVal === "cityslicka"){
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://reqres.in/api/login", true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onreadystatechange = () => {
+          if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+              const response = JSON.parse(xhr.responseText);
+              if(response.token){
+                localStorage.setItem("token", response.token);
+                // Store the email in localStorage
+                localStorage.setItem("email", emailVal);
+                // Display greeting message
+                messageDiv.style.display = "block";
+                messageDiv.textContent = "Hello, " + emailVal;
+                loginForm.style.display = "none";
+                logoutLink.style.display = "block";
+                loggedIn.style.display = "block";
+                loginFormSection.style.display = "none";
+                pageHeader.style.display = "block";
+              } else {
+                // Authentication failed
+                messageDiv.textContent = "Authentication failed. Please try again.";
+                loginFormSection.style.display = "block";
+                messageDiv.style.display = "block";
+                loggedIn.style.display = "none";
+                pageHeader.style.display = "none";
+              }
             }
+          } else {
+            // AJAX request failed
+            messageDiv.textContent = "Error occurred while logging in. Please try again later.";
+            messageDiv.style.display = "block";
           }
-        } else {
-          // AJAX request failed
-          messageDiv.textContent = "Error occurred while logging in. Please try again later.";
-          messageDiv.style.display = "block";
         }
+        const data = JSON.stringify({email: emailVal, password: passwordVal});
+        xhr.send(data);
       }
-      const data = JSON.stringify({email: emailVal, password: passwordVal});
-      
-      xhr.send(data);
+       
     });
 
     logoutLink.addEventListener("click", (e) => {
@@ -76,9 +82,10 @@ document.addEventListener("DOMContentLoaded", function() {
       localStorage.removeItem("email");
       logoutLink.style.display = "none";
       messageDiv.style.display = "none";
-      loginForm.style.display = "block";
-      crudAllUsers.style.display = "none";
+      //loginForm.style.display = "block";
+      loginFormSection.style.display = "block";
       pageHeader.style.display = "none";
+      loggedIn.style.display = "none";
     });
      // login form end
      
@@ -109,16 +116,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const colorValue = document.getElementById('colorValue').value;
     document.getElementById('colorTitle').style.color =  colorValue;
   }
-  function back() {
-    //const crudAllUsers = document.querySelector(".crudAllUsers");
-    //const profileTableContainer = document.querySelector(".profileTableContainer");
-    //const profileTheme = document.querySelector(".profileTheme");
 
-    //crudAllUsers.style.display = "block";
-    //profileTableContainer.style.display = "none";
-    //profileTheme.style.display = "none";
-
-  } 
   function createColor(data) {
     const profileTheme = document.querySelector(".profileTheme");
     profileTheme.innerHTML = `
@@ -139,7 +137,9 @@ document.addEventListener("DOMContentLoaded", function() {
               <td>${data.pantone_value}</td>
           </tr>
       </table>
+      <button class="btn btn-secondary" onclick="back()">BACK</button>
     `
+    
   }
   function updateProfile(userId) {
     const para = 'users/';
@@ -165,17 +165,12 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   }
   function userProfile(userId) {
-    //profileTableContainer, .profileTheme
     const profileTableContainer = document.querySelector(".profileTableContainer");
-    const profileTheme = document.querySelector(".profileTheme");
-    const add_user = document.getElementById("add_user");
-    //const table_id = document.getElementById("table_id_wrapper");
-    //table_id.style.display = "none";
     const crudAllUsers = document.querySelector(".crudAllUsers");
-    //crudAllUsers.style.display = "none";
-    //add_user.style.display = "none";
+    const profileTheme = document.querySelector(".profileTheme");
     profileTableContainer.classList.add("active");
     profileTheme.classList.add("active");
+    crudAllUsers.style.display = "none";
     const para = 'users/';
     const url = baseurl + para + userId;
     fetch(url)
@@ -204,8 +199,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     </tr>
                 </table>
                 <div class="back-btn-container col-xs-12 text-center">
-                    <button class="btn btn-secondary" onclick="updateProfile(${profile.id})">UPDATE</button>
-                   
+                    <button class="btn btn-secondary" onclick="updateProfile(${profile.id})">UPDATE</button>  
                 </div>
         `
       })
@@ -232,7 +226,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     {
                         data: "id",
                         render: function(data){
-                            return " <button class='btn btn-secondary' data-id="+ data +" onclick='profileColor("+ data +");userProfile("+ data +");'>Update</button><button class='btn btn-danger' data-id=" + data + " onclick='deleteUser("+ data +")'>Delete</button>";
+                            return " <button class='btn btn-secondary' data-id="+ data +" onclick='profileColor("+ data +");userProfile("+ data +");'>View Profile</button><button class='btn btn-danger' data-id=" + data + " onclick='deleteUser("+ data +")'>Delete</button>";
                         }
                     },   
                ]
@@ -243,11 +237,4 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   
   loadData();
-  //function addNewUser(first, last) {
-  //  const url = 'https://reqres.in/api/users';
-  //  const data = {
-  //    first_name: firstName,
-  //    last_name: lastName,
-  //  };
-
-  //}
+  
